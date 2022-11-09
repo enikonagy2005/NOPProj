402b45db3f90c9fef57a09f06030aa4009192490
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-11-09 12:43:11 +0100

    1108

--- src/nopproj/NOPProj.java
+++ src/nopproj/NOPProj.java
@@ -15,7 +15,9 @@ public class NOPProj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // addig kér be a felhasználótól egy évszámot, amíg az nem szökőév
+        //kiírja a végén az évszámot fordítva
+        
     }
     
 }
