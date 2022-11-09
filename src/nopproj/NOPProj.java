1d1c3323f6ff3a648513760a5617395ca95302de
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-11-09 13:12:18 +0100

    enikoUwU

--- src/nopproj/NOPProj.java
+++ src/nopproj/NOPProj.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class NOPProj {
     //Szökőév methódus
     public static boolean szokoeve(int ev){
-        boolean a = true;
+        boolean a = false;
         if ((ev > 1582) && (ev % 4 == 0) && (ev % 100 != 0) || (ev % 400 == 0)) {
             a = true;
         }
@@ -25,9 +25,14 @@ public class NOPProj {
     public static void main(String[] args) {
         // addig kér be a felhasználótól egy évszámot, amíg az nem szökőév
         //kiírja a végén az évszámot fordítva
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Adjon meg egy évszámot: ");
         int ev= scanner.nextInt();
-        
+        do {
+            System.out.println("Adj meg egy másik évszámot:");
+            ev = scanner.nextInt();
+        }
+        while (szokoeve(ev) != true );
  
         
     }
