d11b620a56b57569c566caa800490ac913533ca1
K3sTr3L0104 <84494408+K3sTr3L0104@users.noreply.github.com>
2022-11-09 13:47:40 +0100

    nope

--- src/nopproj/NOPProj.java
+++ src/nopproj/NOPProj.java
@@ -24,8 +24,6 @@ public class NOPProj {
     //tetszőleges sorminta
     public static void elkulonito(){
         System.out.println("***************************");
-        System.out.println("******Viktor a király******");
-        System.out.println("***************************");
     }
             public static int main()
                 { int szam;
